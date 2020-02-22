cd6b7020d23463ccb4482fc8f2b02dc0a248b3dd
Kenji Ogura <k5i.ogura.v40@gmail.com>
2020-02-22 21:36:12 +0900

    revise

--- src/convolutional_layer.h
+++ src/convolutional_layer.h
@@ -32,6 +32,7 @@ void update_convolutional_layer(convolutional_layer layer, update_args a);
 image *visualize_convolutional_layer(convolutional_layer layer, char *window, image *prev_weights);
 void binarize_weights(float *weights, int n, int size, float *binary);
 void swap_binary(convolutional_layer *l);
+void swap_ternary(convolutional_layer *l);
 void binarize_weights2(float *weights, int n, int size, char *binary, float *scales);
 
 void backward_convolutional_layer(convolutional_layer layer, network net);
