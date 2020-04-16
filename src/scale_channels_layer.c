296b686a1bc28fae6b8bd8f2a7c3420bc97c9df2
k5iogura <kenji.ogura@jcom.zaq.ne.jp>
2020-04-16 12:22:59 +0900

    "revise"

--- src/scale_channels_layer.c
+++ src/scale_channels_layer.c
@@ -131,7 +131,7 @@ void forward_scale_channels_layer_gpu(const layer l, network net)
     int channel_size = l.out_w * l.out_h;
     int batch_size = l.out_c * l.out_w * l.out_h;
 
-    scale_channels_gpu(net.layers[l.index].output_gpu, size, channel_size, batch_size, l.scale_wh, net.input, l.output_gpu);
+    scale_channels_gpu(net.layers[l.index].output_gpu, size, channel_size, batch_size, l.scale_wh, net.input_gpu, l.output_gpu);
 
     activate_array_gpu(l.output_gpu, l.outputs*l.batch, l.activation);
 }
@@ -146,6 +146,6 @@ void backward_scale_channels_layer_gpu(const layer l, network net)
     float *from_output = net.layers[l.index].output_gpu;
     float *from_delta = net.layers[l.index].delta_gpu;
 
-    backward_scale_channels_gpu(l.delta_gpu, size, channel_size, batch_size, l.scale_wh, net.input, from_delta, from_output, net.delta);
+    backward_scale_channels_gpu(l.delta_gpu, size, channel_size, batch_size, l.scale_wh, net.input_gpu, from_delta, from_output, net.delta_gpu);
 }
 #endif
