0218adea66ac3b7aaee888cef922f42703bfd256
k5iogura <kenji.ogura@jcom.zaq.ne.jp>
2020-05-10 17:08:46 +0900

    "revise"

---
+++ scripts/oi_download.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+wget https://datasets.figure-eight.com/figure_eight_datasets/open-images/train-annotations-bbox.csv
+wget https://datasets.figure-eight.com/figure_eight_datasets/open-images/train-images-boxable.csv
+
+wget https://datasets.figure-eight.com/figure_eight_datasets/open-images/zip_files_copy/validation.zip
+wget https://storage.googleapis.com/openimages/v5/validation-annotations-bbox.csv
+wget https://datasets.figure-eight.com/figure_eight_datasets/open-images/validation-images.csv
+
+wget https://datasets.figure-eight.com/figure_eight_datasets/open-images/zip_files_copy/train_08.zip
+wget https://datasets.figure-eight.com/figure_eight_datasets/open-images/zip_files_copy/train_07.zip
+wget https://datasets.figure-eight.com/figure_eight_datasets/open-images/zip_files_copy/train_05.zip
+wget https://datasets.figure-eight.com/figure_eight_datasets/open-images/zip_files_copy/train_06.zip
+
+wget https://datasets.figure-eight.com/figure_eight_datasets/open-images/zip_files_copy/train_04.zip
+wget https://datasets.figure-eight.com/figure_eight_datasets/open-images/zip_files_copy/train_03.zip
+wget https://datasets.figure-eight.com/figure_eight_datasets/open-images/zip_files_copy/train_02.zip
+wget https://datasets.figure-eight.com/figure_eight_datasets/open-images/zip_files_copy/train_01.zip
+wget https://datasets.figure-eight.com/figure_eight_datasets/open-images/zip_files_copy/train_00.zip
